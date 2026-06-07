dc8cb6f4b8e9faaba38ede8a97594bce9a9c6c44
Furkan <furkan_karatas@endermagazalari.com>
2026-06-07 09:33:33 +0300

    exclude api routes from service worker

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,9 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      workbox: {
+        navigateFallbackDenylist: [/^\/api/]
+      },
       manifest: {
         name: "Furkan's Dashboard",
         short_name: 'Dashboard',
